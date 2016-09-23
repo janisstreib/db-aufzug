9d41ec2e0ea366bdaf1170bcc9c14f7a51983d3a
Janis Streib <janis@dogcraft.de>
2016-09-24 01:42:05 +0200

    FIX: added some foreign keys

--- doc/dbaufzug.sql
+++ doc/dbaufzug.sql
@@ -1,98 +1,83 @@
--- phpMyAdmin SQL Dump
--- version 4.2.12deb2+deb8u1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.6.30, for debian-linux-gnu (x86_64)
 --
--- Host: localhost
--- Generation Time: Jan 10, 2016 at 11:55 PM
--- Server version: 5.5.46-0+deb8u1
--- PHP Version: 5.6.14-0+deb8u1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: dbaufzug
+-- ------------------------------------------------------
+-- Server version	5.6.30-1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `dbaufzug`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `facilities`
 --
 
-CREATE TABLE IF NOT EXISTS `facilities` (
+DROP TABLE IF EXISTS `facilities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `facilities` (
   `id` bigint(11) NOT NULL,
   `type` enum('ELEVATOR','ESCALATOR') COLLATE utf8_bin NOT NULL,
   `description` text COLLATE utf8_bin,
   `locationlat` double NOT NULL,
   `locationlong` double NOT NULL,
-  `station` int(11) NOT NULL
+  `station` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `station` (`station`),
+  CONSTRAINT `facilities_ibfk_1` FOREIGN KEY (`station`) REFERENCES `stations` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `stations`
 --
 
-CREATE TABLE IF NOT EXISTS `stations` (
+DROP TABLE IF EXISTS `stations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stations` (
   `id` bigint(20) NOT NULL,
   `name` varchar(100) NOT NULL,
   `bl` varchar(100) NOT NULL,
   `fern` tinyint(1) NOT NULL,
-  `nah` tinyint(1) NOT NULL
+  `nah` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `stats`
 --
 
-CREATE TABLE IF NOT EXISTS `stats` (
-`id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `stats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stats` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `time` bigint(20) NOT NULL,
-  `facility` int(11) NOT NULL,
-  `state` enum('ACTIVE','INACTIVE','UNKNOWN') COLLATE utf8_bin NOT NULL DEFAULT 'UNKNOWN'
-) ENGINE=InnoDB AUTO_INCREMENT=2417 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `facility` bigint(11) DEFAULT NULL,
+  `state` enum('ACTIVE','INACTIVE','UNKNOWN') COLLATE utf8_bin NOT NULL DEFAULT 'UNKNOWN',
+  PRIMARY KEY (`id`),
+  KEY `facility` (`facility`),
+  CONSTRAINT `stats_ibfk_1` FOREIGN KEY (`facility`) REFERENCES `facilities` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1065034 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `facilities`
---
-ALTER TABLE `facilities`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `stations`
---
-ALTER TABLE `stations`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `stats`
---
-ALTER TABLE `stats`
- ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `stats`
---
-ALTER TABLE `stats`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2417;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-09-24  1:40:37
